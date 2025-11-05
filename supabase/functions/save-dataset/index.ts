1ab47138a11ba38f42d854aabda16de978dfb24f
Nduati-R <ryannduati8@gmail.com>
2025-11-05 10:06:59 +0300

    updates

--- supabase/functions/save-dataset/index.ts
+++ supabase/functions/save-dataset/index.ts
@@ -1,4 +1,4 @@
-import { serve } from "https://deno.land/std@0.168.0/http/server.ts";
+import { serve } from "https://deno.land/std@0.203.0/http/server.ts";
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2.74.0';
 
 const corsHeaders = {
